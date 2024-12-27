c35a588b497002ca5fc10a12f612b6064ec0b390
anh3112003qui <anh.qui0311@gmail.com>
2024-12-27 17:22:47 +0700

    Done contact

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,7 +1,10 @@
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Education from "./components/Education"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Project from "./components/Project"
 
 const App = () =>{
   return (
@@ -10,6 +13,9 @@ const App = () =>{
     <Hero/>
     <About/>
     <Education/>
+    <Project/>
+    <Contact/>
+    <Footer/>
     </>
   )
 }
